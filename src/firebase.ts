bb2532e9606a5357c318a1c909286b7715628289
Qusai Fares <qusaifares@gmail.com>
2020-08-19 14:55:28 -0400

    small fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,6 @@
 import firebase from 'firebase';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCusHGeosMVpiw-5im5NVLZA8W2K8oo_rA',
-  authDomain: 'whatsapp-clone-3d101.firebaseapp.com',
-  databaseURL: 'https://whatsapp-clone-3d101.firebaseio.com',
-  projectId: 'whatsapp-clone-3d101',
-  storageBucket: 'whatsapp-clone-3d101.appspot.com',
-  messagingSenderId: '62586421436',
-  appId: '1:62586421436:web:aeed9649021131d19910b9',
-  measurementId: 'G-49316GG2Z3'
-};
+const firebaseConfig = require('./config/firebaseConfig.json');
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
